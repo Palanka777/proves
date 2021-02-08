73d942a569edbe78b9af8314fb426e16f70bd4b1
Palanka777 <75985553+Palanka777@users.noreply.github.com>
2021-02-08 02:03:37 +0100

    Update E9.java

--- E9.java
+++ E9.java
@@ -1,6 +1,6 @@
 package exercicisjudge;
 
-/*
+/*FICO AQUI UN TEXT DE PROVA PER MODIFICAR UN  ARXIU
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
